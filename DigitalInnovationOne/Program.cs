cca73ec08d43668d4dd6b3342501e93e697f5162
lineker21 <linekerrambaldi99@gmail.com>
2023-01-31 17:01:13 -0300

    feat: first commit

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i =0; i <numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem-Vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
