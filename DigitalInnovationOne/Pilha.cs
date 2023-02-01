33ad5ea6dddc13c012a285edd79c83802155a982
lineker21 <linekerrambaldi99@gmail.com>
2023-02-01 09:19:35 -0300

    feat: second commit

---
+++ DigitalInnovationOne/Pilha.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace DigitalInnovationOne
+{
+    public class Pilha
+    {
+        Posicao primeiro;
+
+        public void Empilha(object item)
+        {
+            primeiro = new Posicao(primeiro, item);
+        }
+
+        public object Desempilha()
+        {
+            if (primeiro == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            object resultado = primeiro.item;
+            primeiro = primeiro.proximo;
+            return resultado;
+        }
+
+        class Posicao
+        {
+            public Posicao proximo;
+
+            public object item;
+
+            public Posicao(Posicao proximo, object item)
+            {
+                this.proximo = proximo;
+                this.item = item;
+            }
+        }
+    }
+}
